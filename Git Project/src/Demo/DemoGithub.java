c3dafa554a3cc650a087b1b8a3c96d107366757b
Sagary123 <99398832+Sagary123@users.noreply.github.com>
2022-02-10 23:36:36 +0530

    Update DemoGithub.java

--- Git Project/src/Demo/DemoGithub.java
+++ Git Project/src/Demo/DemoGithub.java
@@ -6,6 +6,10 @@ public class DemoGithub
   {
      System.out.println("This is m1 method");
   }
+   public void m2()
+  {
+     System.out.println("This is m2 method");
+  }
   public static void main(String[] args)
   {
 	 System.out.println("This is main method");
