c7e4e8218a647e444d3179780805c1ff615f4d6e
Sagar Yeole <Sagar Yeole@DESKTOP-UDADSMO>
2022-02-10 23:23:33 +0530

    by sagar

--- Git Project/src/Demo/Demo.java
+++
@@ -1,9 +0,0 @@
-package Demo;
-
-public class Demo
-{ 
-    public static void main(String[] args)
-    {
-		 System.out.println("Hi this is main method");
-	}
-}
---
+++ Git Project/src/Demo/DemoGit.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class DemoGit 
+{
+   public static void main(String[] args)
+   {
+	  System.out.println("This is main method");
+   }
+}
